9a37222cf4b9e400798e754ea7a3b9389a6630ee
nozomay <n.yahagi1837@gmail.com>
2021-06-12 22:32:43 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '7db6cbc0b48980aa36ea54ce464263b4e8fda3e499887f76264240eec0d2185de8ab9df70f0886f8504c519338ac1c4042119e0a53088ccb8bf39b7f422b677c'
