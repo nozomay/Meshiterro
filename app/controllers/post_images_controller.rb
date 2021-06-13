d4068802d9ce200af8ad345aa495ae9b14a969f5
nozomay <n.yahagi1837@gmail.com>
2021-06-13 00:27:57 +0000

    [Finish]4-4 ページング機能の作成

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -12,7 +12,8 @@ class PostImagesController < ApplicationController
   end
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page]).reverse_order
+    
   end
 
   def show
