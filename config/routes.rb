64ef55cca3d37fa42faf634c64418c4c42f86411
nozomay <n.yahagi1837@gmail.com>
2021-06-13 00:22:06 +0000

    [4-4]いいね機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]do
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+   resource :favorites, only: [:create, :destroy]
    resources :post_comments, only: [:create, :destroy]
-    #post_imagesと親子関係（ネストするという）
+   
   end
 end
