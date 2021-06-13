1547cbbbddc35b871453170bd90c9633fa46ad5d
nozomay <n.yahagi1837@gmail.com>
2021-06-13 04:09:21 +0000

    [First]4-6

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -5,11 +5,14 @@ class PostImagesController < ApplicationController
 
   #投稿データの保存
   def create
-    
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
-    @post_image.save
-    redirect_to post_images_path
+    if @post_image.save
+      redirect_to post_images_path
+    else
+      render :new
+    end
+    
   end
 
   def index
